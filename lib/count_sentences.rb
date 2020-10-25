8c70ad6531b506ca4fafefed1c9301f53e87dd9a
Shilcof <robert.shilcof@gmail.com>
2020-10-25 11:08:23 +0000

    commit tree 467d6efaaeb558de5a7c5941c83bc8b364a6a2c0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    !!self.match(/.*[.]/)
   end
 
   def question?
+    !!self.match(/.*[?]/)
 
   end
 
   def exclamation?
+    !!self.match(/.*[!]/)
 
   end
 
   def count_sentences
-
+    self.scan(/[^.!?]+[.!?]/).count
   end
 end
